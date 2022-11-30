966c447b0d5e3e18f2246b9e9b39b08d896bcd94
Dipta-Sarker <contact.diptasarker@gmail.com>
2022-11-30 19:39:07 +0600

    name added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -5,9 +5,10 @@ import SingleQuiz from '../SingleQuiz/SingleQuiz';
 const Quiz = () => {
     const singleQuiz = useLoaderData()
     const questions = singleQuiz.data.questions;
-    console.log(questions)
+    
     return (
         <div>
+            <h1>{singleQuiz.data.name}</h1>
             {
                 questions.map(quiz => <SingleQuiz key={quiz.id} quiz={quiz}></SingleQuiz>)
             }
