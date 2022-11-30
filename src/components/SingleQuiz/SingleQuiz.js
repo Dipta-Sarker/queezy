a6e1888ef329032a6e7c77ce452c49484b553bdf
Dipta-Sarker <contact.diptasarker@gmail.com>
2022-11-30 18:01:27 +0600

    eye button worked

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './SingleQuiz.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { EyeIcon } from '@heroicons/react/24/solid'
+
 const SingleQuiz = ({quiz}) => {
+    const iconAdded = () =>{
+        const correctAnswer = quiz.correctAnswer;
+        
+        toast(`Correct Answer : ${correctAnswer}`,{position: "top-center"});
+    }
     const addToClickHandler = (id) =>{
         const correctAnswer = quiz.correctAnswer;
         
@@ -20,6 +27,7 @@ const SingleQuiz = ({quiz}) => {
     return (
         <div className='quiz'>
             <h4 className='title'>Quiz:  {quiz.question}</h4>
+            <button onClick={iconAdded} className='btn-icon'><EyeIcon className="icon"/></button>
             <div className='quiz-option'>
                     <button onClick={()=>addToClickHandler(quiz.options[0])} className='btn-quiz'>
                     {quiz.options[0]}
